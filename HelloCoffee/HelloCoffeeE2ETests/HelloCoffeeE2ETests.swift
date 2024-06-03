fb42e1c774ba3ac9cfbab88bb702f4f638b0d8a3
Shuaiqing Luo <luoshuaiqing>
2024-06-02 21:15:56 -0700

    update

---
+++ HelloCoffee/HelloCoffeeE2ETests/HelloCoffeeE2ETests.swift
@@ -0,0 +1,20 @@
+//
+//  HelloCoffeeE2ETests.swift
+//  HelloCoffeeE2ETests
+//
+//  Created by Shuaiqing Luo on 6/2/24.
+//
+
+import XCTest
+
+final class when_app_is_launched_with_no_orders: XCTestCase {
+
+    func test_should_make_sure_no_orders_message_is_displayed() throws {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launchEnvironment = ["ENV": "TEST"]
+        app.launch()
+        
+        XCTAssertEqual("No orders available!", app.staticTexts["noOrdersText"].label)
+    }
+}
