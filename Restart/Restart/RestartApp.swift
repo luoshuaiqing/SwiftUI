7e5b993066e11ed417b8537e7c9a6f9e6bbaaf21
Shuaiqing Luo <luoshuaiqing>
2024-07-06 00:16:03 -0700

    update

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Shuaiqing Luo on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
