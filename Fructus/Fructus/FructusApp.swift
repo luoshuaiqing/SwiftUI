e0e60dc6d6bd5d5fbff7d670053ac02c62de22c3
Shuaiqing Luo <luoshuaiqing>
2024-07-07 12:36:27 -0700

    update

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Shuaiqing Luo on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
