24c7c064976ac5e7e7dc19cad48194633115f698
Shuaiqing Luo <luoshuaiqing>
2024-07-08 09:07:38 -0700

    update

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Shuaiqing Luo on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
