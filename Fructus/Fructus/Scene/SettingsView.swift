b66236f54aa5ef1697109f5dc340ae99d0fe4294
Shuaiqing Luo <luoshuaiqing>
2024-07-08 09:01:47 -0700

    update

--- Fructus/Fructus/Scene/SettingsView.swift
+++ Fructus/Fructus/Scene/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     var body: some View {
         NavigationView {
@@ -33,7 +34,36 @@ struct SettingsView: View {
                     }
                     
                     GroupBox(
-                        label: SettingsLabelView(labelText: "Application", labelImage: "app.iphone")) {
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggling the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding, label: {
+                            if isOnboarding {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                        })
+                        .padding()
+                        .background(
+                            Color(.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")) {
                             SettingsRowView(name: "Developer", content: "John / Jane")
                             SettingsRowView(name: "Designer", content: "Robert Petras")
                             SettingsRowView(name: "Compatibility", content: "iOS 14")
