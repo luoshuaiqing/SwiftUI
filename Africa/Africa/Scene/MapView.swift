0c8262cbd708ea0275b250175f486d088625dbed
Shuaiqing Luo <luoshuaiqing>
2024-07-09 07:50:08 -0700

    update

--- Africa/Africa/Scene/MapView.swift
+++ Africa/Africa/Scene/MapView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        let mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        let mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70, longitudeDelta: 70)
+        let mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("Data/locations.json")
+    
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            MapMarker(coordinate: item.location, tint: .accent)
+        }
     }
 }
 
