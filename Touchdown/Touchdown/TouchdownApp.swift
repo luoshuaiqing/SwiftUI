4926de3b6de6da795d34272d9e292ca8432fea2b
Shuaiqing Luo <luoshuaiqing>
2024-07-11 08:12:54 -0700

    update

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Shuaiqing Luo on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
