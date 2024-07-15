dd0859caa5bf01fa141d534ac24136131f4f0f85
Shuaiqing Luo <luoshuaiqing>
2024-07-15 08:22:05 -0700

    update

--- Touchdown/Touchdown/View/Detail/HeaderDetailView.swift
+++ Touchdown/Touchdown/View/Detail/HeaderDetailView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct HeaderDetailView: View {
+    
+    @Environment(Shop.self) var shop
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 6) {
             Text("Protective Gear")
             
-            Text(sampleProduct.name)
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
--- Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
+++ Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct TopPartDetailView: View {
     
+    @Environment(Shop.self) var shop
+    
     @State private var isAnimating: Bool = false
     
     var body: some View {
@@ -17,7 +19,7 @@ struct TopPartDetailView: View {
                 Text("Price")
                     .fontWeight(.semibold)
                 
-                Text(sampleProduct.formattedPrice)
+                Text(shop.selectedProduct?.formattedPrice ?? sampleProduct.formattedPrice)
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .scaleEffect(1.35, anchor: .leading)
@@ -26,7 +28,7 @@ struct TopPartDetailView: View {
             
             Spacer()
             
-            Image(sampleProduct.image)
+            Image(shop.selectedProduct?.image ?? sampleProduct.image)
                 .resizable()
                 .scaledToFit()
                 .offset(y: isAnimating ? 0 : -35)
@@ -41,4 +43,5 @@ struct TopPartDetailView: View {
 
 #Preview {
     TopPartDetailView()
+        .environment(Shop())
 }
