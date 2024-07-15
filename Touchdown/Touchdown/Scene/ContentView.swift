68598908b4d868908e32a73e966255145a5ce1f5
Shuaiqing Luo <luoshuaiqing>
2024-07-15 08:13:28 -0700

    update

--- Touchdown/Touchdown/Scene/ContentView.swift
+++ Touchdown/Touchdown/Scene/ContentView.swift
@@ -9,43 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(Shop.self) var shop
+    
     var body: some View {
-        VStack(spacing: 0) {
-            NavigationBarView()
-                .padding()
-                .background(.white)
-                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
-            
-            ScrollView(.vertical) {
-                VStack(spacing: 0) {
-                    FeaturedTabView()
-                        .frame(minHeight: 256)
-                        .padding(.vertical, 20)
-                    
-                    CategoryGridView()
-                    
-                    TitleView(title: "Helmets")
-                    
-                    LazyVGrid(columns: gridLayout, spacing: 15) {
-                        ForEach(products) { product in
-                            ProductItemView(product: product)
+        if !shop.showingProduct, shop.selectedProduct == nil {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding()
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                ScrollView(.vertical) {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .frame(minHeight: 256)
+                            .padding(.vertical, 20)
+                        
+                        CategoryGridView()
+                        
+                        TitleView(title: "Helmets")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15) {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            }
                         }
+                        .padding(15)
+                        
+                        TitleView(title: "Brands")
+                        
+                        BrandGridView()
+                        
+                        FooterView()
+                            .padding(.horizontal)
                     }
-                    .padding(15)
-                    
-                    TitleView(title: "Brands")
-                    
-                    BrandGridView()
-                    
-                    FooterView()
-                        .padding(.horizontal)
                 }
             }
+            .background(colorBackground)
+        } else {
+            ProductDetailView()
         }
-        .background(colorBackground)
     }
 }
 
 #Preview {
     ContentView()
+        .environment(Shop())
 }
---
+++ Touchdown/Touchdown/Utility/Shop.swift
@@ -0,0 +1,14 @@
+//
+//  Shop.swift
+//  Touchdown
+//
+//  Created by Shuaiqing Luo on 7/15/24.
+//
+
+import Foundation
+
+@Observable
+class Shop {
+    var showingProduct: Bool = false
+    var selectedProduct: Product? = nil
+}
