e21a80eb1825a51b0ce289ea2a95ec3471ad62cb
Shuaiqing Luo <luoshuaiqing>
2024-07-25 11:57:36 -0700

    update

---
+++ HoneyMoon/HoneyMoon/HoneyMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoonApp.swift
+//  HoneyMoon
+//
+//  Created by Shuaiqing Luo on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
