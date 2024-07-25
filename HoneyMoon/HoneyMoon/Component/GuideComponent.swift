737e16570ebb38aca7580003e6aef28c298f8b00
Shuaiqing Luo <luoshuaiqing>
2024-07-25 13:24:07 -0700

    update

--- HoneyMoon/HoneyMoon/Component/GuideComponent.swift
+++ HoneyMoon/HoneyMoon/Component/GuideComponent.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct GuideComponent: View {
+    
+    var title: String
+    var subtitle: String
+    var description: String
+    var icon: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20, content: {
+            Image(systemName: icon)
+                .font(.largeTitle)
+                .foregroundColor(.pink)
+            
+            VStack(alignment: .leading, spacing: 4, content: {
+                HStack(content: {
+                    Text(title.uppercased())
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    
+                    Spacer()
+                    
+                    Text(subtitle.uppercased())
+                        .font(.footnote)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.pink)
+                })
+                
+                Divider().padding(.bottom, 4)
+                
+                Text(description)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+//                    .fixedSize(horizontal: false, vertical: true)
+            })
+        })
     }
 }
 
 #Preview {
-    GuideComponent()
+    GuideComponent(title: "Title", subtitle: "Swipe right", description: "This is a placeholder sentence. This is a placeholder sentence. This is a placeholder sentence.", icon: "heart.circle")
 }
