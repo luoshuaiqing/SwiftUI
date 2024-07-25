0ec739a930e2ee2f952bbae39f3502f0f6fa232f
Shuaiqing Luo <luoshuaiqing>
2024-07-25 12:29:53 -0700

    update

---
+++ HoneyMoon/HoneyMoon/Model/HoneyMoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneyMoonModel.swift
+//  HoneyMoon
+//
+//  Created by Shuaiqing Luo on 7/25/24.
+//
+
+import Foundation
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
