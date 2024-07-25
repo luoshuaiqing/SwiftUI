97448554ad78f9b98f74f181db21a15689ffcbac
Shuaiqing Luo <luoshuaiqing>
2024-07-25 12:59:02 -0700

    update

---
+++ HoneyMoon/HoneyMoon/View/FooterView.swift
@@ -0,0 +1,43 @@
+//
+//  FooterView.swift
+//  HoneyMoon
+//
+//  Created by Shuaiqing Luo on 7/25/24.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .accentColor(.pink)
+                    .background(
+                        Capsule().stroke(.pink, lineWidth: 2)
+                    )
+            })
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FooterView()
+}
