67f334cd85c10e8a4bd70e2afd14e4b788992a50
Shuaiqing Luo <luoshuaiqing>
2024-07-27 11:39:27 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Profile/ProfileHeaderView.swift
@@ -0,0 +1,76 @@
+//
+//  ProfileHeaderView.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/27/24.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    
+    let user: User
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            // pic and stats
+            HStack {
+                Image(user.profileImageUrl ?? "")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 80, height: 80)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                HStack(spacing: 8) {
+                    UserStatView(value: 3, title: "Posts")
+                    
+                    UserStatView(value: 12, title: "Followers")
+                    
+                    UserStatView(value: 24, title: "Following")
+                }
+            }
+            .padding(.horizontal)
+            
+            // name and bio
+            VStack(alignment: .leading, spacing: 4) {
+                if let fullname = user.fullname {
+                    Text(fullname)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                
+                if let bio = user.bio {
+                    Text(bio)
+                        .font(.footnote)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
+            
+            // action button
+            Button(action: {
+                
+            }, label: {
+                Text("Edit Profile")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 5)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.black)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(.gray, lineWidth: 1)
+                    }
+            })
+            .padding(.horizontal)
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileHeaderView(user: User.MOCK_USERS[0])
+}
--- InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
+++ InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
@@ -20,62 +20,7 @@ struct ProfileView: View {
     var body: some View {
         ScrollView {
             // header
-            VStack(spacing: 10) {
-                // pic and stats
-                HStack {
-                    Image(user.profileImageUrl ?? "")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 8) {
-                        UserStatView(value: 3, title: "Posts")
-                        
-                        UserStatView(value: 12, title: "Followers")
-                        
-                        UserStatView(value: 24, title: "Following")
-                    }
-                }
-                .padding(.horizontal)
-                
-                // name and bio
-                VStack(alignment: .leading, spacing: 4) {
-                    if let fullname = user.fullname {
-                        Text(fullname)
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                    }
-                    
-                    if let bio = user.bio {
-                        Text(bio)
-                            .font(.footnote)
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // action button
-                Button(action: {
-                    
-                }, label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.vertical, 5)
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.black)
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(.gray, lineWidth: 1)
-                        }
-                })
-                .padding(.horizontal)
-                
-                Divider()
-            }
+            ProfileHeaderView(user: user)
             
             // post grid view
             PostGridView(posts: posts)
