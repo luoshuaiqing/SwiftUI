b47e31ed28c899fa7e886a4122465cb628ecd3e6
Shuaiqing Luo <luoshuaiqing>
2024-07-26 18:39:29 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,43 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/26/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
