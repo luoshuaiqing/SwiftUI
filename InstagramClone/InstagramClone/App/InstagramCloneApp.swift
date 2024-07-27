a7e933a563129a7bdbcdb5bc515d28bb4c15fdcd
Shuaiqing Luo <luoshuaiqing>
2024-07-26 18:37:12 -0700

    update

---
+++ InstagramClone/InstagramClone/App/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
