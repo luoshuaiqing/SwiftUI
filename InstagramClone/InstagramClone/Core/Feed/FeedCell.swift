db3bbb5caf8af0d8e9ecf88905ee3b2401aa8c73
Shuaiqing Luo <luoshuaiqing>
2024-07-26 20:44:14 -0700

    update

--- InstagramClone/InstagramClone/Core/Feed/FeedCell.swift
+++ InstagramClone/InstagramClone/Core/Feed/FeedCell.swift
@@ -12,12 +12,49 @@ struct FeedCell: View {
         VStack {
             // image + username
             HStack {
+                Image("placeholder-ironman")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(.circle)
                 
+                Text("batman")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
             }
+            .padding(.leading)
             
             // post image
+            Image("placeholder-ironman")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(.rect)
+            
+            // action buttons
             HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                })
                 
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                })
             }
             
             // likes label
