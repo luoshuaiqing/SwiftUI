0090a5d791ac79db8f01e11d67cb15f0cad0c733
Shuaiqing Luo <luoshuaiqing>
2024-07-26 20:58:36 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Search/SearchView.swift
@@ -0,0 +1,22 @@
+//
+//  SearchView.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/26/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
