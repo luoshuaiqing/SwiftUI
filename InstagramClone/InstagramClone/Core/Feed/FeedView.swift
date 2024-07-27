bae1011d032830d8cca735ef6034f6efa07e2d6e
Shuaiqing Luo <luoshuaiqing>
2024-07-26 20:55:35 -0700

    update

--- InstagramClone/InstagramClone/Core/Feed/FeedView.swift
+++ InstagramClone/InstagramClone/Core/Feed/FeedView.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 32) {
-                ForEach(0...10, id: \.self) { post in
-                    FeedCell()
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { post in
+                        FeedCell()
+                    }
                 }
+                .padding(.top, 8)
             }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram")
+                        .resizable()
+                        .frame(width: 100, height: 50)
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            })
         }
     }
 }
