fd20ed85a2bafbbde2e4a40934e0b7e36839d66c
Shuaiqing Luo <luoshuaiqing>
2024-07-26 20:51:39 -0700

    update

--- InstagramClone/InstagramClone/Core/Feed/FeedView.swift
+++ InstagramClone/InstagramClone/Core/Feed/FeedView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVStack(spacing: 32) {
+                ForEach(0...10, id: \.self) { post in
+                    FeedCell()
+                }
+            }
+        }
     }
 }
 
