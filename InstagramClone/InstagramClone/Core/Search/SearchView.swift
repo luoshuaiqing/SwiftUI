9b3ad15ebf41b8e3a58053cfc2b6cdf8e7136d8d
Shuaiqing Luo <luoshuaiqing>
2024-07-27 08:11:05 -0700

    update

--- InstagramClone/InstagramClone/Core/Search/SearchView.swift
+++ InstagramClone/InstagramClone/Core/Search/SearchView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...15, id: \.self) { user in
+                        HStack {
+                            Image("placeholder-ironman")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(.circle)
+                            
+                            VStack(alignment: .leading, content: {
+                                Text("batman")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Bruce Wayne")
+                            })
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
             }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
