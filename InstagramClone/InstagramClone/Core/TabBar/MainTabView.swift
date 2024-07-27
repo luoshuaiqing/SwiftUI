f64980bedf88668053e8e30a03d1d3c657149ae4
Shuaiqing Luo <luoshuaiqing>
2024-07-27 14:30:35 -0700

    update

--- InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
+++ InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
@@ -8,29 +8,47 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedIndex) {
             FeedView()
+                .onAppear(perform: {
+                    selectedIndex = 0
+                })
                 .tabItem {
                     Image(systemName: "house")
                 }
             
             SearchView()
+                .onAppear(perform: {
+                    selectedIndex = 1
+                })
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
             
             UploadPostView()
+                .onAppear(perform: {
+                    selectedIndex = 2
+                })
                 .tabItem {
                     Image(systemName: "plus.square")
                 }
             
             Text("Notifications")
+                .onAppear(perform: {
+                    selectedIndex = 3
+                })
                 .tabItem {
                     Image(systemName: "heart")
                 }
             
             CurrentUserProfileView(user: User.MOCK_USERS[0])
+                .onAppear(perform: {
+                    selectedIndex = 4
+                })
                 .tabItem {
                     Image(systemName: "person")
                 }
