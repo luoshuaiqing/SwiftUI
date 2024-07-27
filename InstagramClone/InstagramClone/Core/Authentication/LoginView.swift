f6208dcf1e7eaca7387c6b39deec16b6382bccb3
Shuaiqing Luo <luoshuaiqing>
2024-07-27 08:31:29 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,115 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/27/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // logo image
+                Image("instagram")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                })
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(.blue)
+                        .cornerRadius(8)
+                })
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: UIScreen.main.bounds.width / 2 - 40, height: 0.5)
+                    
+                    Text("OR")
+                    
+                    Rectangle()
+                        .frame(width: UIScreen.main.bounds.width / 2 - 40, height: 0.5)
+                }
+                .foregroundStyle(.gray)
+                
+                HStack {
+                    Image("facebook")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.blue)
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                }
+                .padding(.vertical, 16)
+
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
