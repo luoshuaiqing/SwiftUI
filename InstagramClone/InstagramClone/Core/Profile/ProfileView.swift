3d1b7b95acfa3d90b45dd8f484bcf92b0baf2b88
Shuaiqing Luo <luoshuaiqing>
2024-07-26 20:27:19 -0700

    update

--- InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
+++ InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
@@ -11,7 +11,7 @@ struct ProfileView: View {
     var body: some View {
         VStack {
             // header
-            VStack {
+            VStack(spacing: 10) {
                 // pic and stats
                 HStack {
                     Image("placeholder-ironman")
