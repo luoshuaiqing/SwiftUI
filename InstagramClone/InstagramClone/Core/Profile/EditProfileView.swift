0f98e9fac0dea9f325fb2245391fb6718513d059
Shuaiqing Luo <luoshuaiqing>
2024-07-28 11:58:06 -0700

    update

--- InstagramClone/InstagramClone/Core/Profile/EditProfileView.swift
+++ InstagramClone/InstagramClone/Core/Profile/EditProfileView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State private var selectedImage: PhotosPickerItem?
     
     var body: some View {
         VStack {
@@ -42,6 +44,24 @@ struct EditProfileView: View {
             }
             
             // edit profile pic
+            PhotosPicker(selection: $selectedImage) {
+                VStack {
+                    Image(systemName: "person")
+                        .resizable()
+                        .foregroundStyle(.white)
+                        .background(.gray)
+                        .clipShape(.circle)
+                        .frame(width: 80, height: 80)
+                        .scaledToFit()
+                    
+                    Text("Edit Profile Picture")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Divider()
+                }
+            }
+            .padding(.vertical, 8)
             
             // edit profile info
         }
