50d2109d3fee62286d5e31fdc61c363a771d2bba
Shuaiqing Luo <luoshuaiqing>
2024-07-27 17:50:37 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,18 @@
+//
+//  AuthService.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/27/24.
+//
+
+import Foundation
+
+class AuthService {
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+}
