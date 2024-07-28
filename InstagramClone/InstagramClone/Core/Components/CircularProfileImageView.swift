66577857fe0e757bcad66eeba2c1c65dc84d4d96
Shuaiqing Luo <luoshuaiqing>
2024-07-28 16:28:27 -0700

    update

--- InstagramClone/InstagramClone/Core/Components/CircularProfileImageView.swift
+++ InstagramClone/InstagramClone/Core/Components/CircularProfileImageView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 import Kingfisher
 
+enum ProfileImageSize {
+    case xSmall
+    case small
+    case medium
+    case large
+    
+    var dimension: CGFloat {
+        switch self {
+        case .xSmall:
+            return 40
+        case .small:
+            return 48
+        case .medium:
+            return 64
+        case .large:
+            return 80
+        }
+    }
+}
+
 struct CircularProfileImageView: View {
     
     let user: User
+    let size: ProfileImageSize
     
     var body: some View {
         if let imageUrl = user.profileImageUrl {
@@ -30,5 +51,5 @@ struct CircularProfileImageView: View {
 }
 
 #Preview {
-    CircularProfileImageView(user: User.MOCK_USERS[0])
+    CircularProfileImageView(user: User.MOCK_USERS[0], size: .large)
 }
