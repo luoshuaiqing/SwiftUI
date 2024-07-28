b57ff3546ddd492907afb8f7954b9ab4a77adec6
Shuaiqing Luo <luoshuaiqing>
2024-07-27 17:54:33 -0700

    update

--- InstagramClone/InstagramClone/Core/Authentication/Service/AuthService.swift
+++ InstagramClone/InstagramClone/Core/Authentication/Service/AuthService.swift
@@ -6,13 +6,32 @@
 //
 
 import Foundation
+import FirebaseAuth
 
+@Observable
 class AuthService {
+    
+    var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        userSession = Auth.auth().currentUser
+    }
+    
     func login(withEmail email: String, password: String) async throws {
         
     }
     
     func createUser(email: String, password: String, username: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signout() {
         
     }
 }
