629115d0407d4c2d33866a581a2de303b2a0db61
Shuaiqing Luo <luoshuaiqing>
2024-07-28 11:10:38 -0700

    update

--- InstagramClone/InstagramClone/Core/Authentication/ViewModel/RegistrationViewModel.swift
+++ InstagramClone/InstagramClone/Core/Authentication/ViewModel/RegistrationViewModel.swift
@@ -19,5 +19,8 @@ class RegistrationViewModel {
     
     func createUser() async throws {
         try await AuthService.shared.createUser(email: email, password: password, username: username)
+        username = ""
+        email = ""
+        password = ""
     }
 }
