dc8fbda37b5c3066ff479cfbbc344c31d98738a9
Shuaiqing Luo <luoshuaiqing>
2024-07-28 12:06:28 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  EditProfileViewModel.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/28/24.
+//
+
+import Foundation
+import PhotosUI
+import Firebase
+import SwiftUI
+
+@Observable
+class EditProfileViewModel {
+    var selectedImage: PhotosPickerItem? {
+        didSet {
+            Task {
+                await loadImage(fromItem: selectedImage)
+            }
+        }
+    }
+    
+    var profileImage: Image?
+    
+    private func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item else { return }
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let image = UIImage(data: data) else { return }
+        profileImage = Image(uiImage: image)
+    }
+}
