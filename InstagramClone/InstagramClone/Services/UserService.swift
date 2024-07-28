2cf4bc4791c9ec8870f8fd2d92c342de91a1339b
Shuaiqing Luo <luoshuaiqing>
2024-07-28 11:40:23 -0700

    update

--- InstagramClone/InstagramClone/Services/UserService.swift
+++ InstagramClone/InstagramClone/Services/UserService.swift
@@ -11,13 +11,9 @@ import Firebase
 struct UserService {
     
     static func fetchAllUsers() async throws -> [User] {
-        var users = [User]()
         let snapshot = try await Firestore.firestore().collection("users").getDocuments()
-        let documents = snapshot.documents
-        for doc in documents {
-            guard let user = try? doc.data(as: User.self) else { continue }
-            users.append(user)
+        return snapshot.documents.compactMap {
+            try? $0.data(as: User.self)
         }
-        return users
     }
 }
