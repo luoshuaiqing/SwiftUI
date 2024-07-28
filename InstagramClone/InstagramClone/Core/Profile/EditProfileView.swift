7dbeed6af01870614346764da80cd3da7b465a61
Shuaiqing Luo <luoshuaiqing>
2024-07-28 12:01:58 -0700

    update

--- InstagramClone/InstagramClone/Core/Profile/EditProfileView.swift
+++ InstagramClone/InstagramClone/Core/Profile/EditProfileView.swift
@@ -68,6 +68,29 @@ struct EditProfileView: View {
     }
 }
 
+struct EditProfileRowView: View {
+    
+    let title: String
+    let placeholder: String
+    @Binding var text: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .padding(.leading, 8)
+                .frame(width: 100, alignment: .leading)
+            
+            VStack {
+                TextField(placeholder, text: $text)
+                
+                Divider()
+            }
+            .font(.subheadline)
+            .frame(height: 36)
+        }
+    }
+}
+
 #Preview {
     EditProfileView()
 }
