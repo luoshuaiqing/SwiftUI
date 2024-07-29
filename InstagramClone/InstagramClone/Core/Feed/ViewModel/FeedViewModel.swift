3344c46aa6abb265070dc2e4ce899b5eb2a733bd
Shuaiqing Luo <luoshuaiqing>
2024-07-28 18:05:15 -0700

    update

---
+++ InstagramClone/InstagramClone/Core/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewModel.swift
+//  InstagramClone
+//
+//  Created by Shuaiqing Luo on 7/28/24.
+//
+
+import Foundation
+import Firebase
+
+@Observable
+class FeedViewModel {
+    var posts = [Post]()
+    
+    func fetchPosts() async throws {
+        let snapshot = try await Firestore.firestore().collection("posts").getDocuments()
+        posts = try snapshot.documents.compactMap({
+            try $0.data(as: Post.self)
+        })
+    }
+}
