d4b637c21350e4e3d63419cf8d697680688590a1
Shuaiqing Luo <luoshuaiqing>
2024-07-28 18:35:43 -0700

    update

--- InstagramClone/InstagramClone/Core/Components/View/PostGridView.swift
+++ InstagramClone/InstagramClone/Core/Components/View/PostGridView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct PostGridView: View {
     
@@ -26,7 +27,7 @@ struct PostGridView: View {
     var body: some View {
         LazyVGrid(columns: gridItems, spacing: 1, content: {
             ForEach(viewModel.posts) { post in
-                Image(post.imageUrl)
+                KFImage(URL(string: post.imageUrl))
                     .resizable()
                     .scaledToFill()
                     .frame(width: imageDimension, height: imageDimension)
